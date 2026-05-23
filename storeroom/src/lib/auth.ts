cada46d6f22d1aca727ea6c383b52ea5cba51d7b
Yaw Fosu <codehumps00233@gmail.com>
2026-05-23 19:26:57 +0000

    product page

--- storeroom/src/lib/auth.ts
+++ storeroom/src/lib/auth.ts
@@ -1,12 +1,18 @@
 const getToken = () => {
+  if (typeof window === "undefined") return null;
   return localStorage.getItem("token");
 };
+
 const saveToken = (token: string) => {
+  if (typeof window === "undefined") return;
   localStorage.setItem("token", token);
   document.cookie = `token=${token}; path=/; max-age=${60 * 60 * 24 * 7}`;
 };
+
 const deleteToken = () => {
+  if (typeof window === "undefined") return;
   localStorage.removeItem("token");
+  document.cookie = `token=; path=/; max-age=0`;
 };
 
 export { getToken, saveToken, deleteToken };
